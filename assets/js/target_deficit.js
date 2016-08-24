7520d09c7e53b0d53dd61cff341b6c15473009c8
shajjadhossain <shajjadhossain81@gmail.com>
2016-08-24 17:45:05 +0600

    line chart

--- assets/js/target_deficit.js
+++ assets/js/target_deficit.js
@@ -131,24 +131,24 @@ function drawVisualization() {
     //===== For Delivery Qty.
     var data_deficit = google.visualization.arrayToDataTable([
         ['Year',        'Achievement',  {role:'annotation'},    'Goal'],
-        ['Jan 2016',    500,            '8%',                  800],
-        ['Feb 2016',    620,            '15%',                  890],
-        ['Mar 2016',    980,            '9%',                  1130],
-        ['Apr 2016',    1120,            '6%',                  1130],
-        ['May 2016',    1350,            '12%',                  1200],
-        ['Jun 2016',    1400,            '8%',                  1210],
-        ['Jul 2016',    1380,           '6%',                  1220],
-        ['Aug 2016',    1350,           '10%',                  1230],
-        ['Sep 2016',    1320,           '12%',                  1220],
-        ['Oct 2016',    1350,           '5%',                  1210],
-        ['Nov 2016',    1400,           '7%',                  1200],
-        ['Dec 2016',    1600,           '5%',                  1190]
+        ['Jan 2016',    500,            '8%',                  500],
+        ['Feb 2016',    620,            '15%',                  650],
+        ['Mar 2016',    980,            '9%',                  680],
+        ['Apr 2016',    1120,            '6%',                  600],
+        ['May 2016',    1350,            '12%',                  600],
+        ['Jun 2016',    1400,            '8%',                  600],
+        ['Jul 2016',    1380,           '6%',                  650],
+        ['Aug 2016',    1350,           '10%',                  680],
+        ['Sep 2016',    1320,           '12%',                  700],
+        ['Oct 2016',    1350,           '5%',                  680],
+        ['Nov 2016',    1400,           '7%',                  700],
+        ['Dec 2016',    1600,           '5%',                  750]
     ]);
 
     var options_deficit = {
         title: 'Delivery Goods',
         hAxis: {title: 'Years/Time',  titleTextStyle: {color: '#000'}},
-        vAxis: {minValue: 0, maxValue:4000,title:'Delivery(Qty)'},
+        vAxis: {minValue: 0, title:'Delivery(Qty)'},
         //curveType:'function'
         //isStacked: 'percent',
         series:{
@@ -195,24 +195,24 @@ function drawVisualization() {
 
     var data_deficit = google.visualization.arrayToDataTable([
         ['Year',        'Achievement',  {role:'annotation'},    'Goal'],
-        ['Jan 2016',    1000000,            '8%',                  1300000],
-        ['Feb 2016',    1300000,            '15%',                 1380000],
-        ['Mar 2016',    1500000,            '9%',                  1390000],
-        ['Apr 2016',    2000000,            '6%',                  1460000],
-        ['May 2016',    1800000,            '12%',                 1550000],
-        ['Jun 2016',    1600000,            '8%',                  1600000],
-        ['Jul 2016',    1900000,           '6%',                   1700000],
-        ['Aug 2016',    2400000,           '10%',                  1750000],
-        ['Sep 2016',    2700000,           '12%',                  1700000],
-        ['Oct 2016',    3100000,           '5%',                   1800000],
-        ['Nov 2016',    3500000,           '7%',                   1850000],
-        ['Dec 2016',    3300000,           '5%',                   1950000]
+        ['Jan 2016',    1000000,            '8%',                  500000],
+        ['Feb 2016',    1300000,            '15%',                 680000],
+        ['Mar 2016',    1500000,            '9%',                  790000],
+        ['Apr 2016',    1600000,            '6%',                  860000],
+        ['May 2016',    1700000,            '12%',                 950000],
+        ['Jun 2016',    1800000,            '8%',                  1000000],
+        ['Jul 2016',    1900000,           '6%',                   1100000],
+        ['Aug 2016',    2100000,           '10%',                  1250000],
+        ['Sep 2016',    2300000,           '12%',                  1250000],
+        ['Oct 2016',    2500000,           '5%',                   1250000],
+        ['Nov 2016',    2700000,           '7%',                   1250000],
+        ['Dec 2016',    3000000,           '5%',                   1250000]
     ]);
 
     var options_deficit = {
         title: 'Receipt Amount',
         hAxis: {title: 'Years/Time',  titleTextStyle: {color: '#000'}},
-        vAxis: {minValue: 0, maxValue:7000000,title:'Amount(Tk)'},
+        vAxis: {minValue: 0, title:'Amount(Tk)'},
         //curveType:'function'
         //isStacked: 'percent',
         series:{
@@ -260,24 +260,24 @@ function drawVisualization() {
 
     var data_deficit = google.visualization.arrayToDataTable([
         ['Year',        'Achievement',  {role:'annotation'},    'Goal'],
-        ['Jan 2016',    20000,            '8%',                  28000],
-        ['Feb 2016',    25000,            '15%',                 27000],
-        ['Mar 2016',    29000,            '9%',                  26000],
-        ['Apr 2016',    32000,            '6%',                  27500],
-        ['May 2016',    35000,            '12%',                 29000],
-        ['Jun 2016',    33000,            '8%',                  32000],
-        ['Jul 2016',    31000,           '6%',                   33500],
-        ['Aug 2016',    33000,           '10%',                  33000],
-        ['Sep 2016',    35000,           '12%',                  33000],
-        ['Oct 2016',    42000,           '5%',                   32000],
-        ['Nov 2016',    39000,           '7%',                   31000],
-        ['Dec 2016',    37000,           '5%',                   30000]
+        ['Jan 2016',    20000,            '8%',                  22000],
+        ['Feb 2016',    25000,            '15%',                 22000],
+        ['Mar 2016',    29000,            '9%',                  22000],
+        ['Apr 2016',    32000,            '6%',                  22000],
+        ['May 2016',    35000,            '12%',                 22000],
+        ['Jun 2016',    33000,            '8%',                  22000],
+        ['Jul 2016',    31000,           '6%',                   22000],
+        ['Aug 2016',    33000,           '10%',                  22000],
+        ['Sep 2016',    35000,           '12%',                  22000],
+        ['Oct 2016',    42000,           '5%',                   22000],
+        ['Nov 2016',    44000,           '7%',                   22000],
+        ['Dec 2016',    41000,           '5%',                   22000]
     ]);
 
     var options_deficit = {
         title: 'Return : (Re-Saleable)',
         hAxis: {title: 'Years/Time',  titleTextStyle: {color: '#000'}},
-        vAxis: {minValue: 0, maxValue:100000,title:'Return(Qty.)'},
+        vAxis: {minValue: 0, maxValue:45000,title:'Return(Qty.)'},
         //curveType:'function'
         //isStacked: 'percent',
         series:{
@@ -325,24 +325,24 @@ function drawVisualization() {
 
     var data_deficit = google.visualization.arrayToDataTable([
         ['Year',        'Achievement',  {role:'annotation'},    'Goal'],
-        ['Jan 2016',    550,            '2%',                  900],
-        ['Feb 2016',    1000,            '1%',                 1300],
-        ['Mar 2016',    1560,            '3%',                  1450],
-        ['Apr 2016',    1680,            '4%',                  1600],
-        ['May 2016',    1800,            '2%',                 2000],
-        ['Jun 2016',    1900,            '5%',                  2000],
-        ['Jul 2016',    2000,           '6%',                   1800],
-        ['Aug 2016',    2100,           '8%',                  1700],
-        ['Sep 2016',    1950,           '7%',                  1600],
-        ['Oct 2016',    2100,           '9%',                   1580],
-        ['Nov 2016',    2300,           '7%',                   1560],
-        ['Dec 2016',    2400,           '5%',                   1550]
+        ['Jan 2016',    250,            '2%',                  300],
+        ['Feb 2016',    410,            '1%',                 340],
+        ['Mar 2016',    490,            '3%',                  360],
+        ['Apr 2016',    560,            '4%',                  380],
+        ['May 2016',    630,            '2%',                 390],
+        ['Jun 2016',    660,            '5%',                  390],
+        ['Jul 2016',    690,           '6%',                   400],
+        ['Aug 2016',    710,           '8%',                  400],
+        ['Sep 2016',    750,           '7%',                  400],
+        ['Oct 2016',    760,           '9%',                   400],
+        ['Nov 2016',    780,           '7%',                   400],
+        ['Dec 2016',    800,           '5%',                   400]
     ]);
 
     var options_deficit = {
         title: 'Return : (Damage and Broken)',
         hAxis: {title: 'Years/Time',  titleTextStyle: {color: '#000'}},
-        vAxis: {minValue: 0, maxValue:6500,title:'Return(Qty.)'},
+        vAxis: {minValue: 0, title:'Return(Qty.)'},
         //curveType:'function'
         //isStacked: 'percent',
         series:{
@@ -390,24 +390,24 @@ function drawVisualization() {
 
     var data_deficit = google.visualization.arrayToDataTable([
         ['Year',        'Achievement',  {role:'annotation'},    'Goal'],
-        ['Jan 2016',    100,            '2%',                  240],
-        ['Feb 2016',    180,            '1%',                 240],
-        ['Mar 2016',    250,            '3%',                  240],
-        ['Apr 2016',    298,            '4%',                  240],
-        ['May 2016',    350,            '2%',                 240],
-        ['Jun 2016',    340,            '5%',                  240],
-        ['Jul 2016',    300,           '2%',                   240],
-        ['Aug 2016',    270,           '1%',                  240],
-        ['Sep 2016',    240,           '3%',                  240],
-        ['Oct 2016',    280,           '1%',                   240],
-        ['Nov 2016',    340,           '4%',                   240],
-        ['Dec 2016',    360,           '5%',                   240]
+        ['Jan 2016',    10,            '+2%',                  180],
+        ['Feb 2016',    120,            '+10%',                 180],
+        ['Mar 2016',    180,            '+9%',                  180],
+        ['Apr 2016',    258,            '+7%',                  180],
+        ['May 2016',    280,            '+2%',                 180],
+        ['Jun 2016',    300,            '+2%',                  180],
+        ['Jul 2016',    300,           '0%',                   180],
+        ['Aug 2016',    270,           '-2%',                  180],
+        ['Sep 2016',    270,           '-2%',                  180],
+        ['Oct 2016',    280,           '+1%',                   180],
+        ['Nov 2016',    340,           '+4%',                   180],
+        ['Dec 2016',    360,           '+2%',                   180]
     ]);
 
     var options_deficit = {
         title: 'Return : (Manufacture Defects)',
         hAxis: {title: 'Years/Time',  titleTextStyle: {color: '#000'}},
-        vAxis: {minValue: 0, maxValue:1200,title:'Return(Qty.)'},
+        vAxis: {minValue: 0, title:'Return(Qty.)'},
         //curveType:'function'
         //isStacked: 'percent',
         series:{
